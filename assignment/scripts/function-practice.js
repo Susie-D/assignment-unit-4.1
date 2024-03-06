735d20c8414121122a12dda73dada4abdad268aa
Susie Douang <soutsada.d@gmail.com>
2024-03-05 18:37:05 -0600

    complete questions

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -11,61 +11,75 @@ function hello() {
 // Call the function to test
 console.log('Test - should say "Hello World!"', hello());
 
-
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
+let name = 'Susie';
 function helloName(name) {
-  return;
+  return 'Hello, ' + name + '!';
 }
 // Remember to call the function to test
-
+console.log(helloName(name));
 
 // 3. Function to add two numbers together & return the result
-function addNumbers(firstNumber) {
-  // return firstNumber + secondNumber;
+function addNumbers(firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
 }
-
+console.log('Add two numbers:', addNumbers(10, 12));
 
 // 4. Function to multiply three numbers & return the result
-function multiplyThree() {
-
+function multiplyThree(num1, num2, num3) {
+  return num1 * num2 * num3;
 }
+console.log('Result is:', multiplyThree(2, 3, 4));
 
-
-// 5. Function that will return true if a number is positive, 
+// 5. Function that will return true if a number is positive,
 //    or greater than zero, and false otherwise
 function isPositive(number) {
   if (number > 0) {
-    return;
+    return true;
   }
-  return;
+  return false;
 }
-// Call the function to test each outcome (true & false) 
+// Call the function to test each outcome (true & false)
 // Write a separate console.log statement for each outcome
+console.log(isPositive(5));
+console.log(isPositive(-1));
+console.log(isPositive(50));
 
-
-// 6. Function to return the _last_ item in an array. If the 
+// 6. Function to return the _last_ item in an array. If the
 //    array is empty, return `undefined`.
+
 function getLast(array) {
-
+  if (!array || array.length == 0) {
+    return undefined;
+  }
+  let lastItem = array[array.length - 1];
+  return console.log('Last item:', lastItem);
 }
+console.log(getLast([5, 4, 10]));
+console.log('Find nothing and undefined', getLast(''));
 
-// 7. Function to find a value in an array. Return true if the 
+// 7. Function to find a value in an array. Return true if the
 //    value is found and false otherwise. Use a loop;
-//    DO NOT use Array.includes, Array.indexOf, or Array.find 
+//    DO NOT use Array.includes, Array.indexOf, or Array.find
 function find(value, array) {
-
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === value) {
+      return true;
+    }
+  }
+  return false;
 }
 
+console.log('Find 4:', find(4, [1, 5, 6, 8, 4]));
+console.log('Find 17:', find(17, [0, 8, 54, 978, 50]));
 // ----------------------
 // Stretch Goals
 // ----------------------
-// 8. Function to check if a letter is the first letter in a 
+// 8. Function to check if a letter is the first letter in a
 //    string. Return true if it is, and false otherwise
-function isFirstLetter(letter, string) {
-
-}
-
+function isFirstLetter(letter, string) {}
+console.log('Letter check:', isFirstLetter('a', 'orange'));
 
 // 9. Function to return the sum of all numbers in an array
 function sumAll(array) {
@@ -74,19 +88,17 @@ function sumAll(array) {
 
   // TODO: return the sum
 }
+console.log('Return sum of all:', sumAll([5, 10, 12]));
 
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
-function allPositive() {
+function allPositive() {}
 
-}
-
-
-// 11. Pick a problem from Edabit(https://edabit.com/) or 
-//     CodeWars(https://www.codewars.com/). Then describe it 
+console.log('All positive array:', allPositive([5, 10, 12]));
+// 11. Pick a problem from Edabit(https://edabit.com/) or
+//     CodeWars(https://www.codewars.com/). Then describe it
 //     here in a comment, write the function, and test it!
-
 
 // DO NOT MODIFY
 // Used for automated testing
